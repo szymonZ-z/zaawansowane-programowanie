6700ae1b6aa2560126eb0d2d22128b072077306c
Szymon Zackiewicz <zacszym@gmail.com>
2025-12-07 10:20:45 +0100

    ruff test 4

--- zad_1.py
+++ zad_1.py
@@ -2,6 +2,5 @@ def wyswietl_imiona(imiona):
 
     for imie in imiona:
         print(imie)
-
-
-wyswietl_imiona(['Ania', 'Franek', 'Zdzisiław', 'Romek', 'Edward']) 
+#dsf
+wyswietl_imiona(['Ania', 'Franek', 'Zdzisiław', 'Romek', 'Edward'])
